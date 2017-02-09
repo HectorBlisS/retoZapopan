28e618fb65eea935dddcb0a7a48a91adf387bef4
BlisS <contacto@fixter.org>
2017-02-09 17:05:42 -0600

    muro

--- accounts/models.py
+++ accounts/models.py
@@ -11,3 +11,22 @@ class Profile(models.Model):
     	return 'Perfil del usuario {}'.format(self.user.username)
 
 
+class Contact(models.Model):
+	user_from = models.ForeignKey(User, related_name='rel_from_set')
+	user_to = models.ForeignKey(User, related_name='rel_to_set')
+	created = models.DateTimeField(auto_now_add=True, db_index=True)
+
+	class Meta:
+		ordering = ('-created',)
+
+	def __str__(self):
+		return '{} follows {}'.format(self.user_from, self.user_to)
+
+
+
+# Modificamos al modelo User de forma dinamica
+User.add_to_class('following',
+	models.ManyToManyField('self', 
+		through=Contact,
+		related_name='followers',
+		symmetrical=False))
--- muro/views.py
+++ muro/views.py
@@ -1,7 +1,28 @@
 from django.shortcuts import render
 from django.views.generic import View
+from actions.models import Action
+
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
+
 
 
 # Create your views here.
 class Muro(View):
-	pass
+	@method_decorator(login_required)
+	def get(self, request):
+		#mostramos todas las acciones por default
+		actions = Action.objects.exclude(user=request.user)
+		following_ids = request.user.following.values_list('id',flat=True)
+		if following_ids:
+			# si el usuario está siguiendo a otros, filtramos las acciones
+			# actions = actions.filter(user_id__in=following_ids)
+			# optimizando la peticion:
+			actions = actions.filter(user_id__in=following_ids).select_related('user','user__profile').prefetch_related('target')
+		actions = actions[:10]
+		template_name = 'muro/inicio.html'
+		context = {
+			'actions':actions,
+			#'form_user':form_user
+		}
+		return render(request, template_name, context)
