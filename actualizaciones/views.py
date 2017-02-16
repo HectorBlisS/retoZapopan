fa1e7f611685004d491db4abfbaa672b72f409da
BlisS <contacto@fixter.org>
2017-02-16 14:43:58 -0600

    action + actualizaciones

--- actualizaciones/views.py
+++ actualizaciones/views.py
@@ -4,6 +4,10 @@ from .models import Update
 from projects.models import Project
 from django.core.urlresolvers import reverse_lazy
 
+from django.contrib import messages
+
+# crear actions
+from actions.utils import create_action
 
 
 
@@ -15,7 +19,7 @@ class NewUpdate(CreateView):
 	project = None
 
 	def get_success_url(self):
-		return reverse_lazy('dash:detail', args=[self.project.id])
+		return reverse_lazy('actualizaciones:new', args=[self.project.id])
 
 	def dispatch(self, request, pk):
 		self.project = get_object_or_404(Project, id=pk)
@@ -27,3 +31,14 @@ class NewUpdate(CreateView):
 		# the actual modification of the form
 		form.instance.project = self.project
 		return form
+
+	def get_context_data(self, **kwargs):
+		context = super(NewUpdate, self).get_context_data(**kwargs)
+		context['project'] = self.project
+		context['section'] = 'actualizaciones'
+		return context
+
+	def form_valid(self, form):
+		messages.success(self.request, "Tu actualización se ha publicado con éxito")
+		create_action(self.request.user, 'ha publicado una actualizacion de su proyecto',self.project)
+		return super(NewUpdate, self).form_valid(form)  
